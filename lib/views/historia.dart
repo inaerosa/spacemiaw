5df41ef36e059b9cb39818c741c0560fa5d903bb
Inaê Rosa <inaedb.rosa@gmail.com>
2022-11-02 12:31:24 -0300

    style: add border and cat

--- lib/views/historia.dart
+++ lib/views/historia.dart
@@ -15,7 +15,7 @@ class _HistoriaState extends State<Historia> {
   @override
   void initState() {
     super.initState();
-    Timer(Duration(seconds: 10), () {
+    Timer(const Duration(seconds: 10), () {
       Navigator.pushNamed(context, MyHomePage.nomeRota);
     });
   }
@@ -24,31 +24,51 @@ class _HistoriaState extends State<Historia> {
   Widget build(BuildContext context) {
     return Container(
       padding: const EdgeInsets.all(20),
-      // ignore: prefer_const_constructors
-      decoration: BoxDecoration(
-          image: const DecorationImage(
+      decoration: const BoxDecoration(
+          image: DecorationImage(
               image: AssetImage('lib/assets/images/fundo.gif'),
               fit: BoxFit.fill)),
-
-      // ignore: prefer_const_literals_to_create_immutables
-      child: Align(
-        alignment: Alignment.bottomCenter,
-        child: Container(
-          color: Colors.white,
-          width: 400.0,
-          height: 80.0,
-          child: const Text(
-            'AJUDE O GATONAUTA A CAPTURAR O MÁXIMO DE ESTRELAS, FUJA DOS ASTEROIDES...E SE VOCÊ TIVER SORTE, PODE ENCONTRAR UM E.T',
-            textDirection: TextDirection.ltr,
-            style: TextStyle(
-              color: Colors.black,
-              decoration: TextDecoration.none,
-              fontSize: 14,
-              fontFamily: 'Silkscreen',
+      child: Stack(children: [
+        Align(
+          alignment: Alignment.centerLeft,
+          child: Container(
+            margin: const EdgeInsets.only(top: 350),
+            width: 100.0,
+            height: 100.0,
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                fit: BoxFit.fitWidth,
+                image: AssetImage('lib/assets/images/cat.png'),
+              ),
             ),
           ),
         ),
-      ),
+        Align(
+          alignment: Alignment.bottomCenter,
+          child: Container(
+              color: Colors.white,
+              width: 400.0,
+              height: 80.0,
+              child: Container(
+                padding: EdgeInsets.all(5.0),
+                decoration: BoxDecoration(
+                    border: Border.all(
+                        color: Color.fromARGB(255, 255, 7, 181),
+                        width: 5,
+                        style: BorderStyle.solid)),
+                child: const Text(
+                  'AJUDE O GATONAUTA A CAPTURAR O MÁXIMO DE ESTRELAS, FUJA DOS ASTEROIDES...E SE VOCÊ TIVER SORTE, PODE ENCONTRAR UM E.T',
+                  textDirection: TextDirection.ltr,
+                  style: TextStyle(
+                    color: Colors.black,
+                    decoration: TextDecoration.none,
+                    fontSize: 14,
+                    fontFamily: 'Silkscreen',
+                  ),
+                ),
+              )),
+        ),
+      ]),
     );
   }
 }
